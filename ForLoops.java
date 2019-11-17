b8c81b74dd9740d0ade2130be986b577fffcc500
Bullseye198 <57558418+Bullseye198@users.noreply.github.com>
2019-11-17 15:51:53 +0100

    Add files via upload
    Understanding For loops in easy and fun way. Also how to use them with arrays that we just learned.

---
+++ ForLoops.java
@@ -0,0 +1,24 @@
+public class ForLoops {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 10; i++) {	
+			System.out.println(i);
+		}
+	
+	//for loops are useful for printing out all values from an arrays
+		
+	String [] names = {"Martin", "Vlad", "Tomasz"};
+		for (int i = 0; i < names.length; i++) {
+			System.out.println(names[i]);
+			}
+		}
+	}
+	
+/*
+int i = 0;   - this means that it starts from 0
+i < 10;      - this means that this code runs until i is less then 10
+10 - how many times do we want to repeat this code 
+i - it's a variable. It's a good practice to call it i
+i++ - increment i by 1
+*/
