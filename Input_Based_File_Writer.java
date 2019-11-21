aad61b287c09b1dacdcf3f89420f4b2158ab9923
Bullseye198 <57558418+Bullseye198@users.noreply.github.com>
2019-11-22 00:00:48 +0100

    Update Input_Based_File_Writer.java

--- Input_Based_File_Writer.java
+++ Input_Based_File_Writer.java
@@ -1,45 +1,32 @@
-//after making some changes it still doesn't store text into .txt file...
+//Now it works but it doesnt count in spaces.
 
 import java.util.Scanner;
-import java.io.PrintWriter;
-
+import java.io.PrintWriter;	
 
 public class Input_Based_File_Writer {
-	public static PrintWriter writer;
-	
-	public void addRecords() {
-		
-	}
+	static PrintWriter writer;
 	
 	public static void main(String args[]) {
 
 		Scanner keyInput = new Scanner(System.in);
-		String txtName, txtBody = null;		
-        
+		String txtName, txtBody = null;
 
-		
 		try {
-			
-			
-			System.out.println("Enter the name of .txt file: ");
-			txtName = keyInput.nextLine();
-			writer = new PrintWriter(txtName);
-
-			System.out.println("Enter the text body: ");
-			writer.println(txtBody);
-			txtBody = keyInput.nextLine();
-			
-			
-			try {	
+			try {
+				  System.out.println("Enter a name of .txt file: ");
+				  txtName = keyInput.next();
+				writer = new PrintWriter(txtName);
+				System.out.println("Enter a text body: ");
+				txtBody = keyInput.next();
 				writer.println(txtBody);
-			}
-			catch (Exception e1) {
-				System.out.println(e1);
-			}
-		}
+				}
 			catch (Exception e) {
-				System.out.println("The error is" + e);
-		}
-		keyInput.close();
-	}
+					System.out.println("The error is" + e);					
+						}			
+				}
+			finally {
+				writer.close();
+				keyInput.close();
+				}
+			}
 }
